03b1e9131f9530ff5ab1a1db79752f39376d92c0
peada04 <peada04@yahoo.com>
2021-01-07 10:51:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "us-east-2" {
   ami_id_value = "ami-0bbf2c4f0b9b24139" 
 }
   
-#module "eu-west-1" {
-#  source = "app.terraform.io/HC-SE-Onboarding/terramino/aws"  
-#  region = "eu-west-1"
-#  ami_id_value = "ami-035c845062830cd63"
-#}
+module "eu-west-1" {
+  source = "app.terraform.io/HC-SE-Onboarding/terramino/aws"  
+  region = "eu-west-1"
+  ami_id_value = "ami-035c845062830cd63"
+}
