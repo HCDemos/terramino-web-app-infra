9b3ca38a154e0ea4ecf574ccfe6ae3dc3ccf947f
peada04 <peada04@yahoo.com>
2021-01-07 10:47:53 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,12 +7,12 @@ output "us-east-2-url_with_name" {
     value = module.us-east-2.url_with_name
 }
 
-output "eu-west-1-url_by_ip" {
-    description = "url ip to use to view the game"
-    value = module.eu-west-1.url_by_ip
-}
+#output "eu-west-1-url_by_ip" {
+ #   description = "url ip to use to view the game"
+  #  value = module.eu-west-1.url_by_ip
+#}
 
-output "eu-west-1-url_with_name" {
-    description = "url with dns name to use to view the game"
-    value = module.eu-west-1.url_with_name
-}
+#output "eu-west-1-url_with_name" {
+ #   description = "url with dns name to use to view the game"
+ #   value = module.eu-west-1.url_with_name
+#}
