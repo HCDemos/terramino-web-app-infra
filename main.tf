6b66ea9414b19cc66ac1f9fd4478802aa02fc1e7
peada04 <peada04@yahoo.com>
2021-01-07 12:47:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@
 module "us-east-2" {
   source = "app.terraform.io/HC-SE-Onboarding/terramino/aws"
   region = "us-east-2"
-  ami_id_value = "ami-0bbf2c4f0b9b24139" 
+  # the ami_id directly below meets sentinel policy
+  #ami_id_value = "ami-0bbf2c4f0b9b24139" 
+  #the below ami_id is not allowed by aws sentinel policy
+  ami_id_value = "ami-05c2e971"
 }
   
 module "eu-west-1" {
