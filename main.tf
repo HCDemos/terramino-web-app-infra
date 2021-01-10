7142b80be8b3c199fac2384897877e65e046a8d0
peada04 <peada04@yahoo.com>
2021-01-10 09:01:07 -0500

    Update main.tf
    Added deploy us-west-1 and set eu-west-1 to use ami id that is not allowed

--- main.tf
+++ main.tf
@@ -10,8 +10,17 @@ module "us-east-2" {
 module "eu-west-1" {
   source = "app.terraform.io/HC-SE-Onboarding/terramino/aws"  
   region = "eu-west-1"
-  # the ami_id directly below meets sentinel policy
-  ami_id_value = "ami-035c845062830cd63"
+  # the ami_id directly below meets sentinel policy and is in eu-west-1
+  # ami_id_value = "ami-035c845062830cd63"
+  # the below ami_id is not allowed by aws sentinel policy
+  ami_id_value = "ami-05c2e971"
+}
+  
+module "us-west-1" {
+  source = "app.terraform.io/HC-SE-Onboarding/terramino/aws"  
+  region = "us-west-1"
+  # the ami_id directly below meets sentinel policy and is in us-west-1
+  ami_id_value = "ami-03b5d9a16d1261864"  
   #the below ami_id is not allowed by aws sentinel policy
   #ami_id_value = "ami-05c2e971"
 }
